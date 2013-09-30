f9e584f91888fe5c4f308b266bf3a5e637c11098
Raj Vir <raj@rjvir.com>
2013-09-30 17:22:12 -0400

    yo

---
+++ iOSotope/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSotope
+//
+//  Created by Raj Vir on 9/30/13.
+//  Copyright (c) 2013 Raj Vir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSotopeTests/iOSotopeTests.m
@@ -0,0 +1,34 @@
+//
+//  iOSotopeTests.m
+//  iOSotopeTests
+//
+//  Created by Raj Vir on 9/30/13.
+//  Copyright (c) 2013 Raj Vir. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSotopeTests : XCTestCase
+
+@end
+
+@implementation iOSotopeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
